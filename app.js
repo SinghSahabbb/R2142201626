30fbf9077fec8e9924a25229e64ca9ba843aa229
Khyati_Singh <99485337+SinghSahabbb@users.noreply.github.com>
2023-09-10 12:53:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = 8007;
+const port = 7006;
 
 app.use((req, res, next) => {
   req.setTimeout(500);
